cea6329fdf91974551918131828914c326bf174d
zzabinaan <zzabinaan@gmail.com>
2023-01-04 00:57:04 +0700

    add files

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#524F62",
+        background: "#fafafa",
+      },
+      fontFamily: {
+        montserrat: ['"Montserrat"', "sans-serif"],
+        poppins: ['"Poppins"', "sans-serif"],
+      },
+      fontSize: {
+        xs: "0.60rem",
+        sm: "0.65rem",
+        base: "0.8rem",
+        lg: "1rem",
+        xl: "1.3rem",
+        "2xl": "1.65rem",
+      },
+    },
+  },
+  plugins: [],
+};
