a7bf213ad5014d3386821eba124dd3f55a0bccc9
zzabinaan <zzabinaan@gmail.com>
2023-01-06 11:17:33 +0700

    experiences section with js

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,30 @@
 const menuBar = document.querySelector("#menu-bar");
 const navMenu = document.querySelector("#nav-menu");
+const bWork = document.querySelector("#b-work");
+const bEd = document.querySelector("#b-ed");
+const bOt = document.querySelector("#b-ot");
+const workExp = document.querySelector("#work-exp");
+const edExp = document.querySelector("#ed-exp");
+const otExp = document.querySelector("#ot-exp");
 
 menuBar.addEventListener("click", function () {
   menuBar.classList.toggle("menu-bar-active");
   navMenu.classList.toggle("hidden");
+});
+
+bWork.addEventListener("click", function () {
+  workExp.classList.remove("hidden");
+  edExp.classList.add("hidden");
+  otExp.classList.add("hidden");
+});
+bEd.addEventListener("click", function () {
+  workExp.classList.add("hidden");
+  edExp.classList.remove("hidden");
+  otExp.classList.add("hidden");
+});
+
+bOt.addEventListener("click", function () {
+  workExp.classList.add("hidden");
+  edExp.classList.add("hidden");
+  otExp.classList.remove("hidden");
 });
