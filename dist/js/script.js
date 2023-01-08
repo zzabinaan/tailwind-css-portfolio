e58018631e3b82c0bd40296db35503b5fc1d10b1
zzabinaan <zzabinaan@gmail.com>
2023-01-08 19:59:01 +0700

    project overlay

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,6 +5,34 @@ const bOt = document.querySelector("#b-ot");
 const workExp = document.querySelector("#work-exp");
 const otExp = document.querySelector("#ot-exp");
 
+const cUmkmverse = document.querySelector("#card-umkmverse");
+const overlayUmkmverse = document.querySelector("#overlay-umkmverse");
+const overlayUmkmverse2 = document.querySelector("#overlay-umkmverse2");
+
+const cUrievent = document.querySelector("#card-urievent");
+const overlayurievent = document.querySelector("#overlay-urievent");
+const overlayurievent2 = document.querySelector("#overlay-urievent2");
+
+function overlayUmkm(){
+  overlayUmkmverse.classList.remove("hidden");
+  overlayUmkmverse2.classList.remove("hidden");
+}  
+
+function hideoverlayUmkm(){
+  overlayUmkmverse.classList.add("hidden");
+  overlayUmkmverse2.classList.add("hidden");
+}
+
+function overlayUrievent(){
+  overlayurievent.classList.remove("hidden");
+  overlayurievent2.classList.remove("hidden");
+}  
+
+function hideoverlayUrievent(){
+  overlayurievent.classList.add("hidden");
+  overlayurievent2.classList.add("hidden");
+}
+
 menuBar.addEventListener("click", function () {
   menuBar.classList.toggle("menu-bar-active");
   navMenu.classList.toggle("hidden");
