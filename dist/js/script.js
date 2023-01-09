5bab142b42c3195284dd69252601db3364fdde61
zzabinaan <zzabinaan@gmail.com>
2023-01-09 22:03:37 +0700

    add new projects

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,28 +9,58 @@ const cUmkmverse = document.querySelector("#card-umkmverse");
 const overlayUmkmverse = document.querySelector("#overlay-umkmverse");
 const overlayUmkmverse2 = document.querySelector("#overlay-umkmverse2");
 
+const cSmackMouse = document.querySelector("#card-smack-mouse");
+const overlaySmackMouse = document.querySelector("#overlay-smack-mouse");
+const overlaySmackMouse2 = document.querySelector("#overlay-smack-mouse2");
+
 const cUrievent = document.querySelector("#card-urievent");
 const overlayurievent = document.querySelector("#overlay-urievent");
 const overlayurievent2 = document.querySelector("#overlay-urievent2");
 
-function overlayUmkm(){
+const cPortofolio = document.querySelector("#card-portofolio");
+const overlayportofolio = document.querySelector("#overlay-portofolio");
+const overlayportofolio2 = document.querySelector("#overlay-portofolio2");
+const bShow = document.querySelector("#b-show-more");
+const bLess = document.querySelector("#b-show-less");
+
+function overlayUmkm() {
   overlayUmkmverse.classList.remove("hidden");
   overlayUmkmverse2.classList.remove("hidden");
-}  
+}
 
-function hideoverlayUmkm(){
+function hideoverlayUmkm() {
   overlayUmkmverse.classList.add("hidden");
   overlayUmkmverse2.classList.add("hidden");
 }
 
-function overlayUrievent(){
+function overlayUrievent() {
   overlayurievent.classList.remove("hidden");
   overlayurievent2.classList.remove("hidden");
-}  
+}
 
-function hideoverlayUrievent(){
+function hideoverlayUrievent() {
   overlayurievent.classList.add("hidden");
   overlayurievent2.classList.add("hidden");
+}
+
+function overlaySmackmouse() {
+  overlaySmackMouse.classList.remove("hidden");
+  overlaySmackMouse2.classList.remove("hidden");
+}
+
+function hideoverlaySmackmouse() {
+  overlaySmackMouse.classList.add("hidden");
+  overlaySmackMouse2.classList.add("hidden");
+}
+
+function overlayPortofolio() {
+  overlayportofolio.classList.remove("hidden");
+  overlayportofolio2.classList.remove("hidden");
+}
+
+function hideoverlayPortofolio() {
+  overlayportofolio.classList.add("hidden");
+  overlayportofolio2.classList.add("hidden");
 }
 
 menuBar.addEventListener("click", function () {
@@ -50,4 +80,18 @@ bOt.addEventListener("click", function () {
   bOt.classList.add("b-active");
   workExp.classList.add("hidden");
   otExp.classList.remove("hidden");
+});
+
+bShow.addEventListener("click", function () {
+  cSmackMouse.classList.remove("hidden");
+  cPortofolio.classList.remove("hidden");
+  bLess.classList.remove("hidden");
+  bShow.classList.add("hidden");
+});
+
+bLess.addEventListener("click", function () {
+  cSmackMouse.classList.add("hidden");
+  cPortofolio.classList.add("hidden");
+  bLess.classList.add("hidden");
+  bShow.classList.remove("hidden");
 });
